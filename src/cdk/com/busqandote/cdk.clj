be9fb83bb708909ff643311ed123c9dd206277df
Mylab <labs@onmycrowd.com>
2020-03-12 08:29:17 -0700

    Trying to use stedi to deploy to aws

---
+++ src/cdk/com/busqandote/cdk.clj
@@ -0,0 +1,40 @@
+(ns com.busqandote.cdk
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [stedi.cdk.alpha :as cdk]
+            [uberdeps.api :as uberdeps]))
+
+(cdk/import [[App Construct Duration Stack] :from "@aws-cdk/core"]
+            [[Bucket] :from "@aws-cdk/aws-s3"]
+            [[Code Function Runtime Tracing] :from "@aws-cdk/aws-lambda"]
+            [[LambdaRestApi] :from "@aws-cdk/aws-apigateway"])
+
+(def code
+  (let [jarpath "target/app.jar"
+        deps    (edn/read-string (slurp "deps.edn"))]
+    (with-out-str
+      (io/make-parents "target/.")
+      (io/make-parents jarpath)
+      (uberdeps/package deps jarpath {:aliases [:target]}))
+    (Code/fromAsset jarpath)))
+
+(def app (App))
+
+(def stack (Stack app "busqandote-web-lambda"))
+
+(def bucket (Bucket stack "busqandote-web-lambda-bucket"))
+
+(def busqandote-web-fn
+  (Function stack
+            "busqandote-web-fn"
+            {:code        code
+             :handler     "com.busqandote.app/handler"
+             :runtime     (:NODEJS_10_X Runtime)
+             :environment {"BUCKET" (:bucketName bucket)}
+             :memorySize 128
+             :timeout (Duration/seconds 10)
+             }))
+
+(Bucket/grantWrite bucket register-agent-fn)
+
+(def api (LambdaRestApi stack "busqandote-web" {:handler busqandote-web-fn}))
